278e93c310fb5168b9eeb4c672a28a925bbfab20
Mayur Ashishbhai Ajugia <mayurajugia16@gmail.com>
2024-11-08 14:51:32 +0530

    task

---
+++ New/src/Components/New.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <>
+        <div>
+            {/* <h1> New Men Store Start Data 3/11/2024</h1> */}
+
+            <h1>
+                New Men Store 
+                Creating User Innterface
+            </h1>
+        </div>
+    
+    </>
+  )
+}
+
+export default New
